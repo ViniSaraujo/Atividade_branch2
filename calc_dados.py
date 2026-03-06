39c2d621234dc5ba5eb93b5171d597959844a325
VINICIUS DE SOUZA ARAUJO <47627451892@info.local>
2026-03-05 21:57:50 -0300

    Primeiro commit na master

--- calc_dados.py
+++ calc_dados.py
@@ -4,6 +4,6 @@ def validar_vetores ( v1 , v2 ) :
     return True
 
 if __name__ == " __main__ ":
-obs = [1.2 , 2.5 , 3.8]
+    obs = [1.2 , 2.5 , 3.8]
 pred = [1.0 , 2.7 , 4.0]
 print (" Sistema iniciado com sucesso .")
